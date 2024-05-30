6c8725922ed07f65617cd98557db5be862078fa8
Oleksii Budiak <oleksii.budiak@boataround.com>
2024-05-30 10:07:57 +0200

    ADDED: Script to create .env file with parameters from aws parameter store.

---
+++ create-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fetch the database parameters from AWS SSM
+DB_NAME=$(aws ssm get-parameter --name "/Stag/sentry_db_name" --query "Parameter.Value" --output text)
+DB_USER=$(aws ssm get-parameter --name "/Stag/sentry_db_user" --query "Parameter.Value" --output text)
+DB_PASSWORD=$(aws ssm get-parameter --name "/Stag/sentry_db_password" --query "Parameter.Value" --output text)
+DB_HOST=$(aws.ssm get-parameter --name "/Stag/sentry_db_host" --query "Parameter.Value" --output text)
+DB_PORT=$(aws ssm get-parameter --name "/Stag/sentry_db_port" --query "Parameter.Value" --output text)
+
+# Create .env file and write the parameters
+cat <<EOF > .env
+SENTRY_DB_NAME=$DB_NAME
+SENTRY_DB_USER=$DB_USER
+SENTRY_DB_PASSWORD=$DB_PASSWORD
+SENTRY_DB_HOST=$DB_HOST
+SENTRY_DB_PORT=$DB_PORT
+EOF
+
+echo ".env file created with database parameters"

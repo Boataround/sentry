068631f34ee06eac1e0cad5a97d5b79750775203
Oleksii Budiak <oleksii.budiak@boataround.com>
2024-05-30 11:50:29 +0200

    CHANGED: Way of resolving variables and destination file.

--- create-env.sh
+++ create-env.sh
@@ -7,13 +7,28 @@ DB_PASSWORD=$(aws ssm get-parameter --name "/Stag/sentry_db_password" --query "P
 DB_HOST=$(aws ssm get-parameter --name "/Stag/sentry_db_host" --query "Parameter.Value" --output text)
 DB_PORT=$(aws ssm get-parameter --name "/Stag/sentry_db_port" --query "Parameter.Value" --output text)
 
-# Create .env file and write the parameters
-cat <<EOF > .env
-SENTRY_DB_NAME=$DB_NAME
-SENTRY_DB_USER=$DB_USER
-SENTRY_DB_PASSWORD=$DB_PASSWORD
-SENTRY_DB_HOST=$DB_HOST
-SENTRY_DB_PORT=$DB_PORT
-EOF
+# Path to the .env file
+ENV_FILE=".env"
 
-echo ".env file created with database parameters"
+# Function to update or add a variable in the .env file
+update_env_file() {
+    local var_name=$1
+    local var_value=$2
+
+    if grep -q "^$var_name=" "$ENV_FILE"; then
+        # Variable exists, update it
+        sed -i "s/^$var_name=.*/$var_name=$var_value/" "$ENV_FILE"
+    else
+        # Variable does not exist, add it
+        echo "$var_name=$var_value" >> "$ENV_FILE"
+    fi
+}
+
+# Update or add the environment variables
+update_env_file "SENTRY_DB_NAME" "$DB_NAME"
+update_env_file "SENTRY_DB_USER" "$DB_USER"
+update_env_file "SENTRY_DB_PASSWORD" "$DB_PASSWORD"
+update_env_file "SENTRY_DB_HOST" "$DB_HOST"
+update_env_file "SENTRY_DB_PORT" "$DB_PORT"
+
+echo ".env file has been updated."
